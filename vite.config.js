60dcf184feaa958b37a4ef5fbc6631fdf7a6a8e1
Daniil <www.daniil_.ru@mail.ru>
2022-10-13 00:05:01 +0700

    add github-pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 export default defineConfig({
+	publicPath: process.env.NODE_ENV === "production" ? "/product-page/" : "/",
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
